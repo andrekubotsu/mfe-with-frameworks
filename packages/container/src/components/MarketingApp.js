7c94a32402874c2ea91141aae75250f2bcd58b1e
Andre Kubotsu <contato201@gmail.com>
2022-08-19 07:33:00 -0300

    feat: multi tier navigation

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import { mount } from 'marketing/MarketingApp'
+import { useHistory } from 'react-router-dom'
 
 export default () => {
     const ref = React.useRef(null)
+    const history = useHistory()
     
     React.useEffect(()=>{
-        mount(ref.current)
+        const { onParentNavigate } = mount(ref.current, {
+            onNavigate: ({ pathname: nextPathname }) => {
+                const { pathname } = history.location
+                if(pathname !== nextPathname){
+                    history.push(nextPathname)
+                }
+            }
+        })
+
+        history.listen(onParentNavigate)
     }, [])
 
     return <div ref={ref}></div>
